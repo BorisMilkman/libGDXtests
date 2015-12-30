3775e3dff048225449b97c3fb2c06efc86459197
BorisMilkman <borismilkman@gmail.com>
2015-12-30 20:24:08 +0300

    First update :D

--- core/src/com/libgdxtests/game/LibGDXTests.java
+++ core/src/com/libgdxtests/game/LibGDXTests.java
@@ -1,27 +1,21 @@
 package com.libgdxtests.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import com.libgdxtests.game.Tests.MeshCombiningTest;
 
-public class LibGDXTests extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class LibGDXTests extends Game {
+	/* TESTS LIST */
+	public static MeshCombiningTest meshCombiningTest;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		meshCombiningTest = new MeshCombiningTest(this);
+
+		setScreen(meshCombiningTest);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
 	}
 }
---
+++ core/src/com/libgdxtests/game/Tests/MeshCombiningTest.java
@@ -0,0 +1,54 @@
+package com.libgdxtests.game.Tests;
+
+import com.badlogic.gdx.Screen;
+import com.libgdxtests.game.LibGDXTests;
+
+/**
+ * Created by Boris.
+ * Date: 30.12.2015.
+ * Time: 21:20.
+ * This class is being a part of libGDXtests project.
+ * This class was created in com.libgdxtests.game.Tests package.
+ */
+public class MeshCombiningTest implements Screen {
+    LibGDXTests game;
+
+    public MeshCombiningTest(LibGDXTests game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
